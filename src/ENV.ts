b8955f5c99fec4c7eeb813223fab37b86f424c41
Jozef Sovcik <jozef.sovcik@gmail.com>
2022-06-24 17:29:37 +0200

    adding logging

--- src/ENV.ts
+++ src/ENV.ts
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import logger from './logger';
 const yargs = require('yargs');
 
 export const DEBUG = 'mqttc';
@@ -49,20 +50,23 @@ const defaultConfig: FileConfig = {
     clientid: 'wattrouter',
     host: 'tcp://localhost',
   },
-  wattrouter :{
-    host:'http://localhost',
-    interval: 5
-  }
-  
+  wattrouter: {
+    host: 'http://localhost',
+    interval: 5,
+  },
 };
+
+const log = logger.child({ module: 'env' });
 
 export const config = readConfig(argv.config);
 
 export function readConfig(cfgFileName: string): FileConfig {
   const data = fs.readFileSync(cfgFileName, { encoding: 'utf8', flag: 'r' });
+  log.debug('Reading configuration from %s', cfgFileName);
   let cfg: FileConfig = defaultConfig;
   try {
     cfg = JSON.parse(data);
+    log.debug('Configuration=', JSON.stringify(cfg));
   } catch (err) {
     console.error(`Error reading configuration from ${cfgFileName} err=${err}`);
   }
